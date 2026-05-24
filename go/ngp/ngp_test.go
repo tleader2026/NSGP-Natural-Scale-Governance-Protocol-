c55fd93fa8620428814a6fb0109c43b18fc8f12c
Tyler Fischella <tyler@leadory.net>
2026-05-23 23:07:40 -0500

    Use peers scale and NSGP C symbols in go/ngp/ngp_test.go

--- go/ngp/ngp_test.go
+++ go/ngp/ngp_test.go
@@ -5,6 +5,7 @@ import "testing"
 func TestEvaluate(t *testing.T) {
 	inputs := EmptyInputs()
 	inputs[Person] = ScaleInput{StakesDensity: 1.0, Harmony: -0.5, Admissible: true, Weight: 1.0}
+	inputs[Peers] = ScaleInput{StakesDensity: 0.0, Harmony: 0.0, Admissible: true, Weight: 1.0}
 	inputs[Institution] = ScaleInput{StakesDensity: 1.0, Harmony: 0.75, Admissible: true, Weight: 1.0}
 
 	result, err := Evaluate(inputs, Person, Planet)
@@ -20,4 +21,3 @@ func TestEvaluate(t *testing.T) {
 		t.Fatalf("conflict count = %d, want 1", result.ConflictCount)
 	}
 }
-
