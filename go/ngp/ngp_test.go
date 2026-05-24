b90a086cbfd87b1d518534ffd324489d854173cc
Tyler Fischella <tyler@leadory.net>
2026-05-23 22:45:01 -0500

    Add go/ngp/ngp_test.go

---
+++ go/ngp/ngp_test.go
@@ -0,0 +1,23 @@
+package ngp
+
+import "testing"
+
+func TestEvaluate(t *testing.T) {
+	inputs := EmptyInputs()
+	inputs[Person] = ScaleInput{StakesDensity: 1.0, Harmony: -0.5, Admissible: true, Weight: 1.0}
+	inputs[Institution] = ScaleInput{StakesDensity: 1.0, Harmony: 0.75, Admissible: true, Weight: 1.0}
+
+	result, err := Evaluate(inputs, Person, Planet)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if result.Score != 0.25 {
+		t.Fatalf("score = %f, want 0.25", result.Score)
+	}
+
+	if result.ConflictCount != 1 {
+		t.Fatalf("conflict count = %d, want 1", result.ConflictCount)
+	}
+}
+
